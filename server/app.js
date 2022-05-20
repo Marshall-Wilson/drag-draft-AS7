75c01391e7ab1d12960b1c14663c2416e38a8c8c
Marshall-Wilson <72046534+Marshall-Wilson@users.noreply.github.com>
2022-05-20 17:36:30 -0500

    add wildcard routing to react app

--- server/app.js
+++ server/app.js
@@ -19,10 +19,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use('/api', apiRouter);
+
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
 }
-app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
