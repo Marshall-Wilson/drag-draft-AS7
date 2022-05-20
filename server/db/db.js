0dcdefbb6da3e5632ffde53992fda0836b98d0df
Marshall-Wilson <72046534+Marshall-Wilson@users.noreply.github.com>
2022-05-20 17:17:21 -0500

    add rejectUnauthorized: false to ssl config

--- server/db/db.js
+++ server/db/db.js
@@ -8,7 +8,7 @@ const devConfig = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@
 const prodConfig = process.env.DATABASE_URL;
 
 // Initialize sequelize
-const sequelize = new Sequelize((process.env.NODE_ENV === 'production' ? prodConfig : devConfig), { dialect: 'postgres', dialectOptions: { ssl: true } });
+const sequelize = new Sequelize((process.env.NODE_ENV === 'production' ? prodConfig : devConfig), { dialect: 'postgres', dialectOptions: { ssl: { require: true, rejectUnauthorized: false } } });
 
 // Run and update models
 models.userModel(sequelize);
